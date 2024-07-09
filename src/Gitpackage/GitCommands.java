2cad7349b6f2bb8aa67c7e8e792b23bc70f58275
Naveen Neelam <sushmitha.akkala@gmail.com>
2024-07-09 15:57:25 +0530

    GitDemoProject

---
+++ src/Gitpackage/GitCommands.java
@@ -0,0 +1,18 @@
+package Gitpackage;
+
+public class GitCommands {
+
+	public static void main(String[] args) {
+		String folderName="give folder name present in local rep";
+		String Git_init="it will initialise the rep";
+		String Git_add="before commiting adding files to staging area, * represents everything i.e, all files";
+		String Git_Status="To check files present in staging area";
+		String Git_commit="commit the files in staging area----git commit -m first commit";
+		String addToPushCode="git remote add origin url";
+		String Git_push_origin_master="push the code to remote rep";
+		
+		
+
+	}
+
+}
